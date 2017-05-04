0cfe431fab95c480120a23ecfb58021858ce72eb
Michael Scales <mscales@mindwireless.com>
2017-05-04 14:50:17 -0500

    added piping from Popen to browser... working on piping to stdin

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from time import sleep
+from subprocess import Popen, PIPE
+from flask import Flask, render_template, request, Response
+from queue import Queue
 
 
 from automated_sla_tool.src.utilities import valid_dt
-from automated_sla_tool.bin import generic_ui
 
 
 app = Flask(__name__)
@@ -13,16 +15,31 @@ def my_form():
     return render_template('my-form.html')
 
 
-# TODO I want to stop using the terminal and switch to entirely browser based cmd line esque
 @app.route('/', methods=['POST'])
 def main():
-    # This does not pipe the input/output to the browser but to a terminal instead
-    date_time = valid_dt(request.form['date'])
-    return generic_ui.main(report_date=date_time.date())
+    date_time = request.form['date']
+    return index(date_time)
+
+
+# This is wehre I invoke scripting
+def index(string_date=None):
+    def inner():
+        proc = Popen(
+            ['python', 'manual_main.py', string_date],  # call something with a lot of output so we can see it
+            shell=True,
+            stdout=PIPE,
+            stdin=PIPE
+        )
+
+        for line in iter(proc.stdout.readline, ''):
+            sleep(.5)  # Don't need this just shows the text streaming
+            yield '{}{}'.format(line.rstrip().decode('utf-8'), '<br/>\n')
+
+    return Response(inner(), mimetype='text/html')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, port=5000)
 
 
 
--- manual_main.py
+++ manual_main.py
@@ -1,4 +1,9 @@
 from automated_sla_tool.bin.generic_ui import main
+from automated_sla_tool.src.utilities import valid_dt
+
 
 if __name__ == "__main__":
-    main()
+    from sys import argv
+    main(report_date=valid_dt(argv[1]) if len(argv) > 1 else None)
+else:
+    print('entered else')
